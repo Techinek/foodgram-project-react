e97bc400f08c158c6267bec614c358cd72a721d6
Andrey Kuskov <techinek@gmail.com>
2022-04-29 14:59:20 +0300

    Added custom user model

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,7 +119,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
+    email = models.EmailField(verbose_name='Email', unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                             related_name='followers')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                               related_name='following')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                    fields=['user', 'author'], name='unique_follow'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписался на {self.author}'
