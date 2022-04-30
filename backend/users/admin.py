e8483d2a6aba9ee3bcb4138a84091a5fc36f4af3
Andrey Kuskov <techinek@gmail.com>
2022-04-30 17:42:07 +0300

    Modified fields in admin area for CustomUser

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,12 +7,16 @@ User = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'first_name', 'last_name', 'email')
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email',
+                    'role')
     list_filter = ('username', 'email')
+    search_fields = ('username',)
 
 
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('user', 'author')
+    list_display = ('id', 'user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = ('author',)
 
 
 admin.site.register(Follow, FollowAdmin)
