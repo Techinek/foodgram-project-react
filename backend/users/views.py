f68e3f6f8c385b8bee9a5bf8d63bbf59eb0b2214
Andrey Kuskov <techinek@gmail.com>
2022-04-30 17:25:35 +0300

    Copied main logics from previous sprints into current users.views

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,5 +9,59 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from foodgram.pagination import LimitPageNumberPaginator
-from .models import Follow, User
+from .models import Follow, CustomUser
 from .serializers import FollowSerializer
+
+
+@api_view(['POST', 'DELETE'])
+@permission_classes([permissions.IsAuthenticated, ])
+def follow_author(request, pk):
+    user = get_object_or_404(CustomUser, username=request.user.username)
+    author = get_object_or_404(CustomUser, pk=pk)
+
+    if request.method == 'POST':
+        if user.id == author.id:
+            content = {'errors': 'You cannot follow yourself'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            Follow.objects.create(user=user, author=author)
+        except IntegrityError:
+            content = {'errors': 'Author is already in your follows'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        follows = CustomUser.objects.all().filter(username=author)
+        serializer = FollowSerializer(
+            follows,
+            context={'request': request},
+            many=True,
+        )
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        try:
+            subscription = Follow.objects.get(user=user, author=author)
+        except ObjectDoesNotExist:
+            content = {'errors': 'ВAuthor is already in your follows'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        subscription.delete()
+        return HttpResponse('Author is unfollowed',
+                            status=status.HTTP_204_NO_CONTENT)
+
+
+class SubscriptionListView(viewsets.ReadOnlyModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = FollowSerializer
+    pagination_class = LimitPageNumberPaginator
+    filter_backends = (filters.SearchFilter,)
+    permission_classes = (permissions.IsAuthenticated,)
+    search_fields = ('^following__user',)
+
+    def get_queryset(self):
+        user = self.request.user
+        new_queryset = CustomUser.objects.filter(following__user=user)
+        return new_queryset
+
+
+class CustomTokenDestroyView(TokenDestroyView):
+    def post(self, request):
+        utils.logout_user(request)
+        return Response(status=status.HTTP_201_CREATED)
