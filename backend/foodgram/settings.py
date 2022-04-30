cffd172365c1fc030328b75317935b618d41396c
Andrey Kuskov <techinek@gmail.com>
2022-04-30 17:34:51 +0300

    Added urls and included packages in settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'django_filters',
     'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
 ]
