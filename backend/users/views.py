a17cd28be0438af1214c52c9df93166b549b2a68
Andrey Kuskov <techinek@gmail.com>
2022-04-30 17:20:29 +0300

    Added pagination for the project

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from djoser import utils
+from djoser.views import TokenDestroyView
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+from foodgram.pagination import LimitPageNumberPaginator
+from .models import Follow, User
+from .serializers import FollowSerializer
