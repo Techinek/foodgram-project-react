e2391235e47ae2ef7ec0a14c5d40857f6e213542
Andrey Kuskov <techinek@gmail.com>
2022-04-30 18:00:04 +0300

    Added admin area for recipes and ingredients

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingList, Tag)
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    search_fields = ('name',)
+    ordering = ('color',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit', 'get_recipes_count')
+    search_fields = ('name',)
+    ordering = ('measurement_unit',)
+
+    def get_recipes_count(self, obj):
+        return RecipeIngredient.objects.filter(ingredient=obj.id).count()
+
+
+class RecipeIngredientsInline(admin.TabularInline):
+    model = RecipeIngredient
+    min_num = 1
+    extra = 1
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    list_filter = ('id', 'recipe', 'ingredient')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'in_favorite')
+    list_filter = ('name', 'author', 'tags')
+    readonly_fields = ('in_favorite',)
+
+    def in_favorite(self, obj):
+        return obj.in_favorite.all().count()
+
+
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'recipe')
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
