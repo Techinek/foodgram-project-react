d3291134f5897d0c8e8e7bd50793e11038d02aba
Andrey Kuskov <techinek@gmail.com>
2022-05-01 19:57:52 +0300

    Added views for recipes. Added custom function for creating amount of ingredients. Added custom template

---
+++ backend/recipes/utils/shopping.py
@@ -0,0 +1,13 @@
+from django.db.models import F, Sum
+
+from recipes.models import RecipeIngredient
+
+
+def get_shopping_list(user):
+    ingredients = RecipeIngredient.objects.filter(
+        recipe__shopping_recipe__user=user).values(
+        name=F('ingredient__name'),
+        measurement_unit=F('ingredient__measurement_unit')
+    ).annotate(amount=Sum('amount')).values_list(
+        'ingredient__name', 'amount', 'ingredient__measurement_unit')
+    return ingredients
---
+++ backend/recipes/views.py
@@ -0,0 +1,90 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from weasyprint import HTML
+
+from foodgram.pagination import LimitPageNumberPaginator
+from .filters import IngredientFilter, RecipeFilter
+from .models import (Ingredient, Recipe, Tag)
+from .permissions import IsAdminOrReadOnly, IsAuthorOrAdmin
+from .serializers import (
+    AddRecipeSerializer,
+    IngredientSerializer,
+    RecipeSerializer,
+    ShowRecipeSerializer,
+    TagSerializer
+)
+from .utils.shopping import get_shopping_list
+
+
+class RecipesViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_classes = {
+        'retrieve': ShowRecipeSerializer,
+        'list': ShowRecipeSerializer,
+    }
+    default_serializer_class = AddRecipeSerializer
+    permission_classes = (IsAuthorOrAdmin,)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RecipeFilter
+    pagination_class = LimitPageNumberPaginator
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action,
+                                           self.default_serializer_class)
+
+    def _favorite_shopping_post_delete(self, related_manager):
+        recipe = self.get_object()
+        if self.request.method == 'DELETE':
+            related_manager.get(recipe_id=recipe.id).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        if related_manager.filter(recipe=recipe).exists():
+            raise ValidationError('Рецепт уже в избранном')
+        related_manager.create(recipe=recipe)
+        serializer = RecipeSerializer(instance=recipe)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True,
+            permission_classes=[permissions.IsAuthenticated],
+            methods=['POST', 'DELETE'], )
+    def favorite(self, request, pk=None):
+        return self._favorite_shopping_post_delete(
+            request.user.favorite
+        )
+
+    @action(detail=True,
+            permission_classes=[permissions.IsAuthenticated],
+            methods=['POST', 'DELETE'], )
+    def shopping_cart(self, request, pk=None):
+        return self._favorite_shopping_post_delete(request.user.shopping_user)
+
+    @action(detail=False)
+    def download_shopping_cart(self, request):
+        ingredients = get_shopping_list(request.user)
+        html_template = render_to_string('recipes/shopping_template.html',
+                                         {'ingredients': ingredients})
+        html = HTML(string=html_template)
+        result = html.write_pdf()
+        response = HttpResponse(result, content_type='application/pdf;')
+        response['Content-Disposition'] = 'inline; filename=shopping_list.pdf'
+        response['Content-Transfer-Encoding'] = 'binary'
+        return response
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    pagination_class = None
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = IngredientFilter
