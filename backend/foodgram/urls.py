84fc414686808de39eccefc3c20b9eead28a1abc
Andrey Kuskov <techinek@gmail.com>
2022-05-01 20:18:20 +0300

    Added urls for recipes

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls', namespace='api_users')),
+    path('api/', include('users.urls', namespace='api_users')),
+    path('api/', include('recipes.urls', namespace='api_recipes'))
 ]
